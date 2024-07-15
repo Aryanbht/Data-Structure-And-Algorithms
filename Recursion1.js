ea4361d29250f9072e47672c96e1012b041c438a
Aryan <aryanmusicbht@gmail.com>
2024-07-15 15:08:34 +0530

    Another Commit

--- Recursion1.js
+++ Recursion1.js
@@ -3,18 +3,35 @@
 // Output:
 // Fibonacci series of 5 numbers is : 0 1 1 2 3
 
-const range = [0, 1];  
+// const range = [0, 1];  
 
-function fib(num) {  
-  for (let index = 2; index < num; index++) {  
-    range[index] = range[index - 1] + range[index - 2];  
-  }  
-  return range
-}  
+// function fib(num) {  
+//   for (let index = 2; index < num; index++) {  
+//     range[index] = range[index - 1] + range[index - 2];  
+//   }  
+//   return range
+// }  
 
-const num = 10;  
-const fibSeries = fib(num);  
+// const num = 10;  
+// const fibSeries = fib(num);  
 
-console.log(`Fibonacci series up to ${num} terms:`);  
-console.log(fibSeries.join(', '));
+// console.log(`Fibonacci series up to ${num} terms:`);  
+// console.log(fibSeries.join(', '));
 
+// ****************************************************************************************************************
+
+
+// Write a program and recurrence relation to find the Factorial of n where n>2
+
+// function factorial(num) {
+//     if (num==1) {
+//         return 1;
+//     }
+//     if (num==0) {
+//         return 1;        
+//     }
+//     return num * factorial(num-1);
+// }
+
+// let fact = factorial(5)
+// console.log(fact)
